ac4e87b6966d05fa1c0e2c3c99072fb6fa1f9f69
Yasaira Reynoso <reynoso.yasaira@student.greenriver.edu>
2022-02-14 18:59:52 -0500

    Summary page added

--- index.php
+++ index.php
@@ -24,8 +24,30 @@ $f3->route('GET /', function (){
 $f3->route('GET|POST /survey', function ($f3){
     $f3->set('choices', getChoices());
 
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        $_SESSION['fname'] = $_POST['fname'];
+        $_SESSION['choices'] = $_POST['choices'];
+
+        // If condiments were selected
+        if(isset($_POST['choices'])){
+            $_SESSION['choices'] = implode(", ",$_POST['choices']);
+        }
+        // Redirect the user to next page
+        $f3->reroute('summary');
+    }
+
     $view = new Template();
     echo $view->render('views/survey.html');
+});
+
+// Define a summary route
+$f3->route('GET /summary', function (){
+    $view = new Template();
+    echo $view->render('views/summary.html');
+
+    // Clear the session data
+    session_destroy();
 });
 
 // Run fat-free
